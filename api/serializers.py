05c4b585471b4bef397fca541727f6abe9f669f4
Andrew Au <iamandrewau@gmail.com>
2017-02-28 21:32:53 -0500

    add remaining endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from api.models import Products, Users, PantsFitting, ProductCategories, Provinc
 from rest_framework import serializers
 
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Products
         fields = ('id', 'name', 'brand', 'price', 'size_data', 'sku', 'category_id')
@@ -11,13 +11,42 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
 class UsersSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Users
-        fields = ()
+        fields = ('first_name', 
+            'last_name', 
+            'password', 
+            'email', 
+            'phone', 
+            'address',
+            'postal_code',
+            'city',
+            'province',
+            'country',
+            'birth_month',
+            'birth_day',
+            'birth_year',
+            'pants_fit',
+            'shirt_fit',
+            'shirt_size',
+            'pants_size',
+            'shirt_min_price',
+            'shirt_max_price',
+            'pants_min_price',
+            'pants_max_price',
+            'shoes_min_price',
+            'shoes_max_price',
+            'acces_min_price',
+            'acces_max_price',
+            'style_pref',
+            'store_pref',
+            'price_min',
+            'price_max',
+            'referral')
 
 
-class PansFittingSerializer(serializers.HyperlinkedModelSerializer):
+class PantsFittingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Users
-        fields = ()
+        model = PantsFitting
+        fields = ('id', 'pfit_name')
 
 
 class BrandsSerializer(serializers.HyperlinkedModelSerializer):
@@ -29,34 +58,34 @@ class BrandsSerializer(serializers.HyperlinkedModelSerializer):
 class ProductCategoriesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductCategories
-        fields = ()
+        fields = ('id', 'category_name')
 
 
 class ProvincesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Provinces
-        fields = ()
+        fields = ('id', 'province_name')
 
 
 class ShirtFittingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ShirtFitting
-        fields = ()
+        fields = ('id', 'sfit_name')
 
 
 class SizeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Size
-        fields = ()
+        fields = ('id', 'size_name')
 
 
 class StoresSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Stores
-        fields = ()
+        fields = ('id', 'store_name')
 
 
 class StylesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Styles
-        fields = ()
+        fields = ('id', 'style_name')
--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,16 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductsViewSet)
+router.register(r'productcats', views.ProductCategoriesViewSet)
 router.register(r'brands', views.BrandsViewSet)
+router.register(r'pantsfit', views.PantsFitViewSet)
+router.register(r'users', views.UsersViewSet)
+router.register(r'provinces', views.ProvincesViewSet)
+router.register(r'shirtfit', views.ShirtFitViewSet)
+router.register(r'stores', views.StoresViewSet)
+router.register(r'styles', views.StylesViewSet)
+router.register(r'size', views.SizeViewSet)
+
 
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
